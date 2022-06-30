3b4c8de3a4e5d316875a7c79b1de44cfdabff8ad
Robi <robi.maliqi@hotmail.co.uk>
2022-06-30 18:46:09 +0100

    finsihed thermostat

--- 04_thermostat/thermostat.js
+++ 04_thermostat/thermostat.js
@@ -1,11 +1,14 @@
 class Thermostat {
   constructor(){
   this.temperature = 20;
+  this.mode = true;
   }
 
   up(){
-     if (this.temperature < 25){
+     if (this.mode == true && this.temperature < 25){
     this.temperature += 1;
+    } else if (this.mode == false && this.temperature < 32){
+      this.temperature +=1;
     }
   }
 
@@ -15,8 +18,26 @@ class Thermostat {
     }
   }
 
+  setPowerSavingMode(mode){
+    this.mode = mode
+  }
+
+  reset(){
+    this.temperature = 20
+  }
+
   getTemperature(){
     return this.temperature
+  }
+
+  getCurrentUsage(){
+    if (this.temperature < 18) {
+      return "low-usage"
+    } else if (this.temperature <= 25) {
+      return "medium-usage"
+    } else if (this.temperature > 26) {
+      return "high-usage"
+    }
   }
 }
 
@@ -28,21 +49,17 @@ console.log(thermostat.up());
 console.log(thermostat.getTemperature()); // should now return 22
 console.log(thermostat.down());
 console.log(thermostat.getTemperature()); // should now return 21
+console.log(thermostat.setPowerSavingMode(true)); // PSM is now on, max temperature is 25
 
-// thermostat.setPowerSavingMode(true); // PSM is now on, max temperature is 25
+for (let i = 0 ; i < 10 ; i++) {
+  thermostat.up();
+}
 
-// for (let i = 0 ; i < 10 ; i++) {
-//   thermostat.up();
-// }
-
-// thermostat.getTemperature(); // should be 25 (max reached)
-
-// thermostat.setPowerSavingMode(false); // PSM is now off, max temperature is no more 25
-
-// thermostat.up();
-// thermostat.getTemperature(); // should now return 26
-
-// thermostat.reset();
-// thermostat.getTemperature(); // should be back to 20
+console.log(thermostat.getTemperature()); // should be 25 (max reached)
+console.log(thermostat.setPowerSavingMode(false)); // PSM is now off, max temperature is no more 25
+console.log(thermostat.up());
+console.log(thermostat.getTemperature()); // should now return 26
+console.log(thermostat.reset());
+console.log(thermostat.getTemperature()); // should be back to 20
 
 module.exports = Thermostat;
--- 04_thermostat/thermostat.test.js
+++ 04_thermostat/thermostat.test.js
@@ -47,5 +47,66 @@ describe('thermostat', () => {
       }
       expect(thermostat.getTemperature()).toEqual(25)
     });
+
+    it('checks if power saving mode is on', () => {
+      const thermostat = new Thermostat();
+      thermostat.setPowerSavingMode(true);
+      expect(thermostat.mode).toBeTruthy();
+    })
+
+    it('the maximum temperature is 25 degrees, when power saving mode is on', () => {
+      const thermostat = new Thermostat();
+      thermostat.setPowerSavingMode(true);
+      for(let i = 0 ; i < 20 ; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getTemperature()).toEqual(25)
+    });
+
+    it('if power saving mode is off then max temperature is 32', () => {
+      const thermostat = new Thermostat();
+      thermostat.setPowerSavingMode(false);
+      for(let i = 0 ; i < 20 ; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getTemperature()).toEqual(32)
+    });
+  });
+  describe('./reset', () => {
+    it('resets the temperature back to default', () => {
+      const thermostat = new Thermostat();
+      thermostat.up()
+      thermostat.reset()
+      expect(thermostat.getTemperature()).toEqual(20)
+    });
+  });
+  describe('./getCurrentUsage', () => {
+    it('returns "low-usage" if the temp is < 18', () => {
+      const thermostat = new Thermostat();
+      for(let i = 0 ; i < 3 ; i++) {
+        thermostat.down();
+      }
+      expect(thermostat.getCurrentUsage()).toEqual("low-usage")
+      expect(thermostat.getTemperature()).toEqual(17)
+    });
+
+    it('returns "medium-usage" if the temp is <= 25', () => {
+      const thermostat = new Thermostat();
+      for(let i = 0 ; i < 4 ; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentUsage()).toEqual("medium-usage")
+      expect(thermostat.getTemperature()).toEqual(24)
+    });
+
+    it('returns "high-usage" if the temp is > 25', () => {
+      const thermostat = new Thermostat();
+      thermostat.setPowerSavingMode(false);
+      for(let i = 0 ; i < 10 ; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentUsage()).toEqual("high-usage")
+      expect(thermostat.getTemperature()).toEqual(30)
+    });
   });
 });
