162cb7d899bb83597a92ab2c7be73a4260ee6738
Robi <robi.maliqi@hotmail.co.uk>
2022-06-30 22:07:21 +0100

    Exercise 3: Create a function fetchRepositoryInfo (in fetchRepositoryInfo.js) which; fetches repository data from Github's API, calls the given callback with the received data (as a JS object).

--- 05_calling_apis/githubRequest.js
+++ 05_calling_apis/githubRequest.js
@@ -1,40 +1,30 @@
 
+// Load the `got` function.
 const got = require('got');
 
+// Create a "handler" callback function.
 const handleReceivedResponse = (response) => {
-  const responseObject = JSON.parse(response.body);
-  console.log(responseObject);
+  console.log(response.body);
 }
 
-got('https://api.github.com/repos/sinatra/sinatra').then(handleReceivedResponse);
+const url = 'https://api.github.com/repos/sinatra/sinatra';
 
-
-// // Load the `got` function.
-// const got = require('got');
-
-// // Create a "handler" callback function.
-// const handleReceivedResponse = (response) => {
-//   console.log(response.body);
-// }
-
-// const url = 'https://api.github.com/repos/sinatra/sinatra';
-
-// // Call `got` and provide the handler callback function.
-// // This will get called by `got` when the response is received.
-// got(url).then(handleReceivedResponse);
+// Call `got` and provide the handler callback function.
+// This will get called by `got` when the response is received.
+got(url).then(handleReceivedResponse);
 
 // We can also rewrite the above using a shorter version,
 // by defining the "handler" function as an anonymous function
 
-// got(url)
-//   .then((response) => {
-//     console.log(response.body);
-//   });
+got(url)
+  .then((response) => {
+    console.log(response.body);
+  });
 
-// const stringValue = '{ "name": "John" }';
-// const objectValue = JSON.parse(stringValue);
+const stringValue = '{ "name": "John" }';
+const objectValue = JSON.parse(stringValue);
 
-// console.log(objectValue); // { name: 'John' }
+console.log(objectValue); // { name: 'John' }
 
-// // We can access properties like a regular JS object
-// console.log(objectValue.name);
+//  We can access properties like a regular JS object
+console.log(objectValue.name);
